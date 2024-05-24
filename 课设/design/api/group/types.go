57287ccc1fa1ffbc6bfa328d59d360c2acb49ffb
RLVxx001 <154057174+RLVxx001@users.noreply.github.com>
2024-05-24 20:13:59 +0800

    2024/5/24 22

--- 课设/design/api/group/types.go
+++ 课设/design/api/group/types.go
@@ -2,28 +2,8 @@ package group
 
 import (
 	"design/domain/group"
-	"github.com/gorilla/websocket"
-	"net/http"
 	"time"
 )
-
-var clients = make(map[uint][]*websocket.Conn) //消息专用
-var broadcast = make(chan GroupMessage)
-
-var clientNews = make(map[uint][]*websocket.Conn) //创建
-var broadcastNew = make(chan GroupUser)           //
-
-var clientRes = make(map[uint][]*websocket.Conn) //撤回
-var broadcastRe = make(chan GroupMessage)
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		// 允许跨域请求（仅作为示例，生产环境请考虑安全性）
-		return true
-	},
-} // 使用默认的WebSocket升级选项
 
 type GroupRequest struct {
 	Id            uint           `json:"id"`            //群id
